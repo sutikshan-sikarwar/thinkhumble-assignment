6b9f3c4be5a7e1c06e34204ad75029ddf29cd734
sutikshan-sikarwar <sutikshan.sikarwar@gmail.com>
2025-05-02 21:50:49 +0530

    debugging

--- src/main/java/com/example/thinkhumble/config/SecurityConfig.java
+++ src/main/java/com/example/thinkhumble/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/v3/api-docs",
                                 "/api-docs/**",
-                                "/api-docs")
+                                "/api-docs",
+                                "/api/quotes/**")
                         .permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
